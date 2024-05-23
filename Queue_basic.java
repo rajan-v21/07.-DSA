a26942aa03767d9adfed2290007cd8510f560ada
Rajan Vishwakarma <rajanvishwakarma218@gmail.com>
2024-05-23 09:39:56 +0530

    day 2 started

---
+++ Queue_basic.java
@@ -0,0 +1,56 @@
+import java.util.*;
+public class Queue_basic {
+    int front, rear, size;
+    int capacity;
+    int arr[];
+    void createQueue(int capacity){
+        this.capacity = capacity;
+        front = rear = 0;
+        arr = new int[capacity];
+    }
+    boolean isFull(){
+        return rear == capacity;
+    }
+    boolean isEmpty(){
+        return rear == front;
+    }
+    void enqueue(int item){
+        if(isFull()){
+            System.out.println("Queue is full");
+            return;
+        }
+        arr[rear] = item;
+        rear++;
+    }
+    int dequeue(){
+        if(isEmpty()){
+            System.out.println("Queue is empty");
+            return -1;
+        }
+        int item = arr[front];
+        front++;
+        return item;
+    }
+    void printQueue(){
+        for(int i = front; i < rear; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue_basic q = new Queue_basic();
+        q.createQueue(5);
+        q.enqueue(10);
+        q.enqueue(20);
+        q.enqueue(30);
+        q.enqueue(40);
+        q.enqueue(50);
+        q.printQueue();
+        System.out.println();
+        System.out.println(q.dequeue());
+        System.out.println(q.dequeue());
+        System.out.println(q.dequeue());
+        System.out.println(q.dequeue());
+        System.out.println(q.dequeue());
+    }
+}
